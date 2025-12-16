78937ada5ac857951171aeba04c0b7c11d5676ce
kiwi61634-del <kiwi61634@gmail.com>
2025-12-16 14:55:41 +0900

    Entity class 설계 + h2 Table 생성 / JPA 환경설정

---
+++ src/main/java/com/mysite/sbb/Answer.java
@@ -0,0 +1,24 @@
+package com.mysite.sbb;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDate createDate;
+
+    @ManyToOne
+    private Question question;
+}
